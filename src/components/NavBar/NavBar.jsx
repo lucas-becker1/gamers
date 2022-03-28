c958c0e1fbf05c1fe043bce47856e53b4ddfae29
lucas.becker <lucas.becker.14.lb@gmail.com>
2022-03-28 14:01:54 -0300

    color navbar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,28 +1,23 @@
 import React from 'react';
 import { Link } from "react-router-dom";
-import { Navbar, Nav, NavLink } from 'react-bootstrap';
 import './NavBar.css';
 import logo from '../../images/logo2.png'
 import CartWidget from '../CartWidget/CartWidget';
 
 const NavBar = () => {
     return (
-        <div>
-    <Navbar bg="light" variant="light" expand="md" collapseOnSelect>
-       <Navbar.Brand className='mx-4' as={Link} to="/"> <img className='mx-1' src={logo} alt="logo"/>
-            </Navbar.Brand>
-            <Navbar.Toggle className='mx-2'/>
-            <Navbar.Collapse>
-            <Nav className="mx-auto fs-4">
-                <Nav.Link  className='mx-4' as={Link} to="/productos/FIFA 22">FIFA 22</Nav.Link>
-                <Nav.Link  className='mx-4' as={Link} to="/productos/NBA 2K22">NBA 2K22</Nav.Link>
-                <Nav.Link  className='mx-4' as={Link} to="/productos/ELDER RING">ELDER RING</Nav.Link>
-            </Nav>
-            </Navbar.Collapse>
-            <NavLink as={Link} to="/cart"> <CartWidget /> </NavLink>
-    </Navbar>
-    </div>
- )
-}
+        <header className="header">
+            
+        <Link className="header-link" as={Link} to="/"> <img src={logo} alt="logo"/></Link>
+
+        <nav className="header-nav">
+            <Link className="header-link" to="/productos/FIFA 22">FIFA 22</Link>
+            <Link className="header-link" to="/productos/NBA 2K22">NBA 2K22</Link>
+            <Link className="header-link" to="/productos/ELDER RING">ELDER RING</Link>
+            <CartWidget/>
+        </nav>
+    </header>
+  );
+};
 
 export default NavBar
